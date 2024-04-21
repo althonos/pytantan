03a85a3de01d8b0ba4c8f54a58203cb9a65d2807
Martin Larralde <martin.larralde@embl.de>
2024-04-21 11:45:03 +0200

    Add initial project folder

---
+++ pytantan/__init__.py
@@ -0,0 +1,16 @@
+# noqa: D104
+from ._version import __version__
+
+__author__ = "Martin Larralde <martin.larralde@embl.de>"
+__license__ = "GPLv3+"
+__all__ = [
+    "Alphabet",
+    "Aligner",
+    "BaseDatabase",
+    "Database",
+    "ScoreMatrix",
+    "ScoreResult",
+    "EndResult",
+    "FullResult",
+    "align",
+]
