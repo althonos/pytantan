862d00c9dc0aac4db6ec490912e91df52f4fcee3
Martin Larralde <martin.larralde@embl.de>
2024-04-21 22:02:01 +0200

    Update `ScoreMatrix.__init__` to support arbitrary matrix creation

--- pytantan/lib.pyx
+++ pytantan/lib.pyx
@@ -4,7 +4,9 @@
 from libc.math cimport exp
 from libc.limits cimport INT_MAX
 from libc.stdlib cimport calloc, free
+from libcpp.vector cimport vector
 
+from .tantan cimport SCORE_MATRIX_SIZE
 from .tantan.options cimport TantanOptions, OutputType
 from .tantan.alphabet cimport Alphabet as _Alphabet, DNA, PROTEIN
 from .tantan.score_matrix cimport ScoreMatrix as _ScoreMatrix, BLOSUM62
@@ -20,6 +22,10 @@ from pytantan.platform.generic cimport maskSequencesNone
 #     from pytantan.platform.sse4 cimport maskSequencesSSE4
 # if AVX2_BUILD_SUPPORT:
 #     from pytantan.platform.avx2 cimport maskSequencesAVX2
+
+cdef extern from "<cctype>" namespace "std" nogil:
+    cdef int toupper(int ch)
+    cdef int tolower(int ch)
 
 
 cdef class Alphabet:
@@ -51,24 +57,14 @@ cdef class Alphabet:
 
 cdef class ScoreMatrix:
     cdef readonly Alphabet     alphabet
-    cdef          _ScoreMatrix scoring
 
     cdef double*  probMatrix
     cdef double** probMatrixPointers
     cdef int*     fastMatrix
     cdef int**    fastMatrixPointers
 
-    def __init__(self, Alphabet alphabet not None):
-        # record alphabet
-        self.alphabet = alphabet
-
-        # make scoring matrix
-        if alphabet.letters == PROTEIN.decode():
-            unstringify(self.scoring, BLOSUM62)
-        else:
-            self.scoring.initMatchMismatch(1, 1, alphabet.letters.encode('ascii'))
-
-        # allocate matrices
+    cdef int _allocate_matrix(self) except 1:
+        # allocate data and pointer arrays
         self.probMatrix = <double*> calloc(sizeof(double), 64*64)
         if self.probMatrix is NULL:
             raise MemoryError
@@ -81,35 +77,83 @@ cdef class ScoreMatrix:
         self.fastMatrixPointers = <int**> calloc(sizeof(int*), 64)
         if self.fastMatrixPointers is NULL:
             raise MemoryError
-
-        # prepare pointers
+        # prepare pointers to data
         for i in range(64):
             self.fastMatrixPointers[i] = &self.fastMatrix[i * 64]
             self.probMatrixPointers[i] = &self.probMatrix[i * 64]
-        for i in range(64*64):
-            self.fastMatrix[i] = 0
-            self.probMatrix[i] = 0.0
+        return 0
 
-        # compute fast matrix
-        self.scoring.makeFastMatrix(
-            self.fastMatrixPointers,
-            64,
-            self.alphabet._abc.lettersToNumbers,
-            self.scoring.minScore(),
-            False
-        )
+    cdef int _make_fast_matrix(self, vector[vector[int]]& scores) except 1:
+        cdef size_t i
+        cdef size_t j
+        cdef int    a
+        cdef int    b
+        cdef int    c
+        cdef int    d
+        cdef int    x
+        cdef int    y
+        cdef int    default = INT_MAX
+
+        # find default score
+        for i in range(self.alphabet._abc.size):
+            for j in range(self.alphabet._abc.size):
+                if scores[i][j] < default:
+                    default = scores[i][j]
+
+        # fill matrix with default score (FIXME)
+        for i in range(SCORE_MATRIX_SIZE):
+            for j in range(SCORE_MATRIX_SIZE):
+                self.fastMatrixPointers[i][j] = default
+
+        # update matrix with scores
+        for i in range(self.alphabet._abc.size):
+            x = self.alphabet._abc.numbersToLetters[i]
+            a = self.alphabet._abc.lettersToNumbers[toupper(x)]
+            b = self.alphabet._abc.lettersToNumbers[tolower(x)]
+            
+            for j in range(self.alphabet._abc.size):
+                y = self.alphabet._abc.numbersToLetters[j]
+                c = self.alphabet._abc.lettersToNumbers[toupper(y)]
+                d = self.alphabet._abc.lettersToNumbers[tolower(y)]
+
+                self.fastMatrixPointers[a][c] = scores[i][j]
+                self.fastMatrixPointers[a][d] = scores[i][j]
+                self.fastMatrixPointers[b][c] = scores[i][j]
+                self.fastMatrixPointers[b][d] = scores[i][j]
+
+    def __init__(self, Alphabet alphabet not None, object matrix not None):
+        cdef double              lambda_  
+        cdef vector[vector[int]] scores  = vector[vector[int]]()
+        cdef LambdaCalculator    lc      = LambdaCalculator()
+
+        # record alphabet
+        self.alphabet = alphabet
+
+        # extract scores
+        if len(matrix) != self.alphabet._abc.size:
+            raise ValueError("Matrix length should be equal to alphabet length")
+        scores.resize(alphabet._abc.size)
+        for i, row in enumerate(matrix):
+            scores[i].resize(alphabet._abc.size)
+            if len(row) != alphabet._abc.size:
+                raise ValueError("Matrix width should be equal to alphabet length")
+            for j, x in enumerate(row):
+                scores[i][j] = x
+
+        # prepare fast matrix
+        self._allocate_matrix()
+        self._make_fast_matrix(scores)
 
         # compute lambda factor
-        cdef LambdaCalculator lc = LambdaCalculator()
         lc.calculate(self.fastMatrixPointers, self.alphabet._abc.size)
         if lc.isBad():
             raise RuntimeError("cannot calculate probabilities for score matrix")
 
         # compute likelihood matrix
-        cdef double matrixLambda = lc.lambda_()
+        lambda_ = lc.lambda_()
         for i in range(64):
             for j in range(64):
-                x = matrixLambda * self.fastMatrixPointers[i][j]
+                x = lambda_ * self.fastMatrixPointers[i][j]
                 self.probMatrixPointers[i][j] = exp(x)
 
 
--- pytantan/tantan/__init__.pxd
+++ pytantan/tantan/__init__.pxd
@@ -13,5 +13,11 @@ cdef extern from "tantan.hh" namespace "tantan" nogil:
         double minMaskProb,
         const unsigned char* maskTable
     ) except +
-    
+
+
+cdef extern from * nogil:
+    """
+    const size_t SCORE_MATRIX_SIZE = 64;
+    """
+    const size_t SCORE_MATRIX_SIZE
 
