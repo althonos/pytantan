ff97c55765bee0fd47b1705abe2ba9d0c7ea0bb0
Martin Larralde <martin.larralde@embl.de>
2024-04-21 19:14:41 +0200

    Add convenience methods for creating alphabets and scoring matrices

--- pytantan/lib.pyx
+++ pytantan/lib.pyx
@@ -7,7 +7,7 @@ from libc.stdlib cimport calloc, free
 
 from .tantan.options cimport TantanOptions, OutputType
 from .tantan.alphabet cimport Alphabet as _Alphabet, DNA, PROTEIN
-from .tantan.score_matrix cimport ScoreMatrix as _ScoreMatrix
+from .tantan.score_matrix cimport ScoreMatrix as _ScoreMatrix, BLOSUM62
 from .tantan.utils cimport unstringify
 from .tantan.lc cimport LambdaCalculator
 
@@ -23,42 +23,59 @@ if SSE4_BUILD_SUPPORT:
 
 cdef class Alphabet:
     cdef _Alphabet _abc
+    cdef bint      _protein
 
     @classmethod
     def dna(cls):
-        cdef Alphabet abc = cls.__new__(cls)
-        abc._abc.fromString(DNA)
-        return abc
+        return cls(DNA, protein=False)
 
     @classmethod
-    def protein(cls):
-        cdef Alphabet abc = cls.__new__(cls)
-        abc._abc.fromString(PROTEIN)
-        return abc
+    def amino(cls):
+        return cls(PROTEIN, protein=True)
+
+    def __init__(self, object letters not None, bint protein = False):
+        if isinstance(letters, str):
+            letters = letters.encode('ascii')
+        self._protein = protein
+        self._abc.fromString(letters)
+
+    @property
+    def letters(self):
+        cdef size_t i
+        return ''.join(
+            chr(self._abc.numbersToLetters[i])
+            for i in range(self._abc.size)
+        )
 
 
 cdef class ScoreMatrix:
     cdef readonly Alphabet     alphabet
     cdef          _ScoreMatrix scoring
 
-    cdef double* probMatrix
+    cdef double*  probMatrix
     cdef double** probMatrixPointers
-    cdef int* fastMatrix
-    cdef int** fastMatrixPointers
+    cdef int*     fastMatrix
+    cdef int**    fastMatrixPointers
 
-    def __init__(self, Alphabet alphabet not None):
-        # 
-        self.alphabet = alphabet      
+    def __init__(self, object alphabet not None):
+        #
+        if not isinstance(alphabet, Alphabet):
+            alphabet = Alphabet(alphabet)
+        self.alphabet = alphabet
 
         # make scoring matrix
-        self.scoring.initMatchMismatch(1, 1, b"ACGTU")
+        if alphabet.letters == PROTEIN.decode():
+            unstringify(self.scoring, BLOSUM62)
+        else:
+            self.scoring.initMatchMismatch(1, 1, alphabet.letters.encode('ascii'))
 
+        # allocate matrices
         self.probMatrix = <double*> calloc(sizeof(double), 64*64)
         if self.probMatrix is NULL:
             raise MemoryError
         self.probMatrixPointers = <double**> calloc(sizeof(double*), 64)
         if self.probMatrixPointers is NULL:
-            raise MemoryError      
+            raise MemoryError
         self.fastMatrix = <int*> calloc(sizeof(int), 64*64)
         if self.fastMatrix is NULL:
             raise MemoryError
@@ -66,6 +83,7 @@ cdef class ScoreMatrix:
         if self.fastMatrixPointers is NULL:
             raise MemoryError
 
+        # prepare pointers
         for i in range(64):
             self.fastMatrixPointers[i] = &self.fastMatrix[i * 64]
             self.probMatrixPointers[i] = &self.probMatrix[i * 64]
@@ -73,19 +91,22 @@ cdef class ScoreMatrix:
             self.fastMatrix[i] = 0
             self.probMatrix[i] = 0.0
 
+        # compute fast matrix
         self.scoring.makeFastMatrix(
-            self.fastMatrixPointers, 
-            64, 
-            self.alphabet._abc.lettersToNumbers, 
-            self.scoring.minScore(), 
+            self.fastMatrixPointers,
+            64,
+            self.alphabet._abc.lettersToNumbers,
+            self.scoring.minScore(),
             False
         )
 
+        # compute lambda factor
         cdef LambdaCalculator lc = LambdaCalculator()
         lc.calculate(self.fastMatrixPointers, self.alphabet._abc.size)
         if lc.isBad():
             raise RuntimeError("cannot calculate probabilities for score matrix")
-        
+
+        # compute likelihood matrix
         cdef double matrixLambda = lc.lambda_()
         for i in range(64):
             for j in range(64):
@@ -102,7 +123,7 @@ cdef class Tantan:
     def __init__(
         self,
         ScoreMatrix score_matrix not None,
-        *, 
+        *,
         bint protein = False,
     ):
         # store score matrix and alphabet
@@ -111,7 +132,7 @@ cdef class Tantan:
         # initialize options
         self._options.isProtein = protein
         if self._options.maxCycleLength < 0:
-            self._options.maxCycleLength = 50 if protein else 100
+            self._options.maxCycleLength = 50 if self.alphabet._protein else 100
         if self._options.mismatchCost == 0:
             self._options.mismatchCost = INT_MAX
 
@@ -121,21 +142,21 @@ cdef class Tantan:
             sequence = bytearray(sequence, 'ascii')
         elif not isinstance(sequence, bytearray):
             sequence = bytearray(sequence)
-        
+
         cdef unsigned char[::1] seq = sequence
 
         self.alphabet._abc.encodeInPlace(&seq[0], &seq[seq.shape[0]])
         maskSequencesSSE4(
             &seq[0],
             &seq[seq.shape[0]],
-            100,
+            self._options.maxCycleLength,
             <const double**> self.score_matrix.probMatrixPointers,
-            0.005,
-            0.05,
-            0.9,
+            self._options.repeatProb,
+            self._options.repeatEndProb,
+            self._options.repeatOffsetProbDecay,
             0.0, #firstGapProb,
             0.0, #otherGapProb,
-            0.5,
+            self._options.minMaskProb,
             self.alphabet._abc.numbersToLowercase,
         )
 
